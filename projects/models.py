87c71e0e758f4418265927e6a3376735e9f0f6a5
KacperBabiak <93874406+KacperBabiak@users.noreply.github.com>
2024-11-18 20:25:06 +0100

    Added new pages

--- projects/models.py
+++ projects/models.py
@@ -1,8 +1,13 @@
 from django.db import models
+from ckeditor.fields import RichTextField
+
 
 class Project(models.Model):
-    title = models.CharField(max_length=100)
-    description = models.TextField()
-    technology = models.CharField(max_length=20)
+    title = models.CharField(max_length=200,default="domyślny tytuł")
+    description = models.TextField(default="domyślny tekst")
+    content = RichTextField(default="domyślny tekst")
     image = models.FileField(upload_to="project_images/", blank=True)
+    
+    def __str__(self):
+        return self.title
 
